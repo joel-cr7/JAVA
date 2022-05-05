fe7776f11fd86f8ac800a90e9bab5c845f256d6d
Joel Miranda <79870303+joel-cr7@users.noreply.github.com>
2022-05-05 18:01:54 +0530

    Add files via upload

---
+++ Assignments/PrimitiveDatatypes.java
@@ -0,0 +1,21 @@
+package HelloWorld;
+
+public class PrimitiveDatatypes {
+
+	public static void main(String[] args) {
+		byte myByte1 = 127;
+		long myLong1 = 100000000L;
+		int myNum = 90000;
+		float myFloat1=1000.666f;
+		double myDouble1 = 48976.8987d;
+		boolean myBool = true;
+		short myShort = 6000;
+		System.out.println("Int datatype: " + myNum);
+		System.out.println("Byte datatype: " + myByte1);
+		System.out.println("Long datatype: " + myLong1);
+		System.out.println("FLoat datatype: " + myFloat1);
+		System.out.println("Short datatype: " + myShort);
+		System.out.println("Double datatype: " +myDouble1);
+	}
+
+}
