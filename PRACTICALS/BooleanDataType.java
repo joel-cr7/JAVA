ed628c12dc65c3b5aadf2690abfdc45ce4b749be
Joel Miranda <79870303+joel-cr7@users.noreply.github.com>
2022-05-05 17:24:38 +0530

    Add files via upload

---
+++ PRACTICALS/BooleanDataType.java
@@ -0,0 +1,17 @@
+package HelloWorld;
+
+public class BooleanDataType
+
+{
+
+public static void main(String args[])
+
+{
+
+boolean myBool = true;
+
+if(myBool == true)
+
+System.out.println("I am using a Boolean data type:"+myBool);}
+
+}
